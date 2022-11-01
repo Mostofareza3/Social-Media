c2ec5ae612e127ed048278ce2e8f84db0965996c
Mostofareza3 <86896410+Mostofareza3@users.noreply.github.com>
2022-11-01 22:33:15 +0600

    router-setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+
 function App() {
-  return <div>welcome to frontend</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import "./styles/icons/icons.css";
 import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./redux/rootReducer";
+import { Provider } from "react-redux";
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
