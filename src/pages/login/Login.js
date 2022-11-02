46176a216a4747a1e61d09ef65d73be04f3669b2
Mostofareza3 <86896410+Mostofareza3@users.noreply.github.com>
2022-11-02 23:40:17 +0600

    login page start

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,7 +1,44 @@
+import { Formik, Form } from "formik";
 import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
 
 const Login = () => {
-  return <div>Login Page</div>;
+  return (
+    <div className="login">
+      <div className="login_wrapper">
+        <div className="login_wrap">
+          <div className="login-1">
+            <img src="../../icons/facebook.svg" alt="" />
+            <span>
+              Facebook helps you connect and share with the people in your life.
+            </span>
+          </div>
+          <div className="login-2">
+            <div className="login-2_wrap">
+              <Formik>
+                {(formik) => (
+                  <Form>
+                    <input type="text" />
+                    <input type="text" />
+                    <button type="submit">Login</button>
+                  </Form>
+                )}
+              </Formik>
+              <Link to="/forgot">Forgotten password?</Link>
+              <div className="sign_splitter"></div>
+              <button className="blue_btn open_signup">Create Account</button>
+            </div>
+            <Link to="/">
+              <b>Create a page</b>
+              for a celebrity, brand or business.
+            </Link>
+          </div>
+        </div>
+        <div className="register"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
