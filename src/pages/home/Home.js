23991f0a0a5771c43fde490dcb95f75d4c76218b
Mostofareza3 <86896410+Mostofareza3@users.noreply.github.com>
2022-11-20 23:10:39 +0600

    logout done

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,17 +4,20 @@ import CreatePost from "../../components/createPost/CreatePost";
 import Header from "../../components/header/Header";
 import LeftHome from "../../components/home/left/LeftHome";
 import RightHome from "../../components/home/right";
+import SendVerification from "../../components/home/sendVerification";
 import Stories from "../../components/home/stories";
 import "./style.css";
 
 export default function Home() {
-  const { user } = useSelector((user) => ({ ...user }));
+  const { user } = useSelector((state) => ({ ...state }));
+
   return (
     <div className="home">
       <Header />
       <LeftHome user={user} />
       <div className="home_middle">
         <Stories />
+        {!user.verified && <SendVerification user={user} />}
         <CreatePost user={user} />
       </div>
       <RightHome user={user} />
