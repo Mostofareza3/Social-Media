9e92bbfbbbdc63246f9751ea8e58a8dc12c2de7a
unknown <mostofa.reza3@gmail.com>
2023-06-04 23:57:41 +0600

    ready for deploy

--- src/index.js
+++ src/index.js
@@ -7,9 +7,8 @@ import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./reducers";
-const store = createStore(rootReducer, composeWithDevTools());
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
